3459eed13e18309fc61471b122efa9ba3caddb0c
Crowdbotics <team@crowdbotics.com>
2023-01-24 20:09:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "roadrepair-38439",
+  url: "https://roadrepair-38439.botics.co",
+  api: "https://roadrepair-38439.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
